604b642c5f9125626a34aa9607f49df6a03e72d2
christina030 <80701591+christina030@users.noreply.github.com>
2025-12-28 13:25:52 +0800

    Update eval.sh

--- eval/eval.sh
+++ eval/eval.sh
@@ -5,7 +5,7 @@ python evaluate.py --data_path ../Generated/result --metric DTW
 python evaluate.py --data_path ../Generated/result --metric Content_score --pretrained_model evaluators/mainuscript/trad_CS_UWSC_best.pth
 
 # GS
-python gs.py --gen_dir ../Generated/result/test2
+python gs.py --gen_dir ../Generated/result/test2 --real_dir ../Generated/result/test2_gt
 
 # FID
 python -m pytorch_fid ../Generated/result/test2_gt ../Generated/result/test2
