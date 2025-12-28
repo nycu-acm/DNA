3a98cba9cf4c8f91188e17e2ddfce8ac2f9901bb
christina030 <80701591+christina030@users.noreply.github.com>
2025-12-28 13:11:38 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,8 @@
 #UWSC
-CUDA_VISIBLE_DEVICES=0 python test.py -c character_dict_t_only --sample_size all --store_type both --unseen --dir Generated/result --pretrained_model Saved/best.pth
+CUDA_VISIBLE_DEVICES=0 python test.py -c character_dict_t_only --sample_size all --store_type both --save_dir Generated/result --model Saved/best.pth
 
 #UWUC
-CUDA_VISIBLE_DEVICES=0 python test.py -c character_dict_t_only_unseen --dataset 1 --sample_size all --store_type both --unseen --dir Generated/result_unseen --pretrained_model Saved/best.pth
+CUDA_VISIBLE_DEVICES=0 python test.py -c character_dict_t_only_unseen --dataset 1 --sample_size all --store_type both --save_dir Generated/result_unseen --model Saved/best.pth
 
 
 
